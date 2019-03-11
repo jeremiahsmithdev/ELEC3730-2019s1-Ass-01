f505375c4bfa593bcaa5937f8d9899bb3dfd0a9b
Unknown <xolopx@gmail.com>
2019-03-11 23:41:01 +1100

    Literally just effed about with Eclipse.
    It was a mission

--- ELEC3730_Ass1/Ass-01/Src/Ass-01-Q01.c
+++ ELEC3730_Ass1/Ass-01/Src/Ass-01-Q01.c
@@ -1,16 +1,14 @@
-//     $Date: 2019-01-05 04:50:09 +1100 (Sat, 05 Jan 2019) $
-// $Revision: 1529 $
-//   $Author: Peter $
+/* QUESTION ONE */
 
-// Question 1
-// Continuing Question One (TOM)
 
 #include "Ass-01.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int read_coefficients(int *coeff_num_p, double **coeff_values_p,
                       char *filename) {
 	FILE *file;
+
 	// Open file for reading
 	if ((file = fopen(filename, "rb")) == NULL)
 	{
@@ -32,41 +30,6 @@ int read_coefficients(int *coeff_num_p, double **coeff_values_p,
 
 	// passes back a pointer to the filter coefficients pointed to by coeff_values_p.
 	coeff_values_p = &coeff;
-	/* *coeff_values_p = coeff; */
 
-	// print values (test)
-	for (int i = 0; i < coeffNum; i++) { printf("%f\n", coeff[i]); }
-
-	/* for (int i = 0; i < coeffNum; i++) { printf("%f\n", &coeff_values_p[i]); } */
-
-	  return 0;
+	return *coeff_num_p;
 }
-
-int main()
-{
-	read_coefficients(0, 0, "filterBP.bin");
-	// test code here
-}
-// count file size...
-
-// reverse into little endian format?
-/* coeffNum = (coeffNum >> 24) | (coeffNum >> 8 & 0x0000FF00) | (coeffNum << 8 & 0x00FF0000) | (coeffNum << 24); */
-
-
-/* printf("%f\n", *coeff_values_p[0]); */
-/* printf("last... %f\n", *coeff_values_p[2]); */
-
-//– Little Endian: Increasing numerical significance with increasing memory address.
-/* (Examples are: x86, 8051, NIOS). */
-
-/* while ( fread( coeff_num_p, 4, 1, file) != 0) */
-/* { */
-/* coeff_values_p += 1; */
-/* } */
-/*  */
-/* if (fread(&coeff_num_p, sizeof(coeff_num_p), 1, file) != 1) */
-/* { */
-/* 	printf("ERROR: Reading double from file\n"); */
-/* 	fclose(file); */
-/* 	return -1; */
-/* } */
--- ELEC3730_Ass1/Ass-01/Src/Ass-01-Q04.c
+++ ELEC3730_Ass1/Ass-01/Src/Ass-01-Q04.c
@@ -4,12 +4,38 @@
 
 // Question 4
 
+// Ask about "zero length strings".
+
 #include "Ass-01.h"
 
 int string_parser(char *inp, char **array_of_words_p[]) {
-  //
-  // WRITE CODE HERE
-  //
-  printf("\nCODE TO BE WRITTEN...\n");
-  return 0;
+
+  // Need to parse a terminal input into individual words.
+  // The input is NULL terminated.
+  // Needs to return the number of individual words in the input.
+
+  int *num_of_words = 0;                      // The number of words in the input.
+  char delim[] = " ";                         // The delimiter for breaking up the input into words.
+  int init_size = strlen(inp);                // The length of the input.
+  char copiedInput[init_size];
+  strncpy(copiedInput,inp,init_size);
+
+
+  char *ptr = strtok(copiedInput, delim);// strtok returns a pointer to the word
+
+  while(ptr != NULL)                          // Keep breaking the input up until NULL is reached at the end.
+  {
+    printf("'%s'\n", ptr);
+    ptr = strtok(NULL, delim);                // NULL is specified as the input to broken up due to the way strtok works.
+    num_of_words++;
+}
+
+  // This loop will show that there are zeroes in the str after tokenizing
+  for (int i = 0; i < init_size; i++)
+  {
+    printf("%d ", inp[i]); // Convert the character to integer, in this case the character's ASCII equivalent
+  }
+  printf("\n");
+
+  return *num_of_words;
 }
