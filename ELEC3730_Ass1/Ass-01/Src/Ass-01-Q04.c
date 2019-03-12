307db227413bea90b8acffe53e27c5d572abcd2f
Unknown <xolopx@gmail.com>
2019-03-12 23:25:05 +1100

    Yeah Q4 not working. I think it's my malloc use...

--- ELEC3730_Ass1/Ass-01/Src/Ass-01-Q04.c
+++ ELEC3730_Ass1/Ass-01/Src/Ass-01-Q04.c
@@ -1,41 +1,38 @@
-//     $Date: 2019-01-05 04:50:09 +1100 (Sat, 05 Jan 2019) $
-// $Revision: 1529 $
-//   $Author: Peter $
-
 // Question 4
 
-// Ask about "zero length strings".
-
 #include "Ass-01.h"
+#include <stdio.h>
 
 int string_parser(char *inp, char **array_of_words_p[]) {
 
-  // Need to parse a terminal input into individual words.
-  // The input is NULL terminated.
-  // Needs to return the number of individual words in the input.
 
-  int *num_of_words = 0;                      // The number of words in the input.
-  char delim[] = " ";                         // The delimiter for breaking up the input into words.
-  int init_size = strlen(inp);                // The length of the input.
-  char copiedInput[init_size];
-  strncpy(copiedInput,inp,init_size);
+  const char delim[2] = " ";                            //The delimiter for breaking up the string.
+  char *token;                                          //Intermediate storage of words.
+  char* string = strdup(inp);                           //NULL terminated string copy of inp.
+  array_of_words_p = malloc(sizeof(char)*100);          //Allocate some memory for the array of words. This amount matches the input string "inp".
+  int count = 1;
 
 
-  char *ptr = strtok(copiedInput, delim);// strtok returns a pointer to the word
+  token = strtok(string,delim);       //Initial breaking of the string to get first word.
+  strcpy(*array_of_words_p[0],token); //Store initial words in the array of words.
 
-  while(ptr != NULL)                          // Keep breaking the input up until NULL is reached at the end.
-  {
-    printf("'%s'\n", ptr);
-    ptr = strtok(NULL, delim);                // NULL is specified as the input to broken up due to the way strtok works.
-    num_of_words++;
-}
 
-  // This loop will show that there are zeroes in the str after tokenizing
-  for (int i = 0; i < init_size; i++)
-  {
-    printf("%d ", inp[i]); // Convert the character to integer, in this case the character's ASCII equivalent
+  while( token !=  NULL){
+
+    printf("\n%s", token);
+
+    token = strtok(NULL, delim);            //Store the next broken word from the string.
+   // strcpy(*array_of_words_p[count],token); //Copy the word into the array of words.
+    count++;
   }
-  printf("\n");
 
-  return *num_of_words;
+/*
+    printf("%s ", *array_of_words_p[0]);
+    printf("%s ", *array_of_words_p[1]);
+    printf("%s ", *array_of_words_p[2]);
+    printf("\n");
+    i++;
+*/
+
+  return 0;
 }
